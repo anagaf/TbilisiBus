604238e40c43c007ede0c1318bc1495d9b331d26
anagaf <aagafonov@arrival.com>
2023-05-23 19:35:00 +0400

    Fixed buses response parser test

--- app/src/test/java/com/anagaf/tbilisibus/data/ttc/BusesResponseParserTest.kt
+++ app/src/test/java/com/anagaf/tbilisibus/data/ttc/BusesResponseParserTest.kt
@@ -3,27 +3,28 @@ package com.anagaf.tbilisibus.data.ttc
 import com.anagaf.tbilisibus.data.Bus
 import com.anagaf.tbilisibus.data.Location
 import com.anagaf.tbilisibus.data.Buses
+import com.anagaf.tbilisibus.data.Direction
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class TtlResponseDeserializerTest {
+class BusesResponseParserTest {
     @Test
     fun deserialize() {
         val stream = Thread.currentThread()
             .contextClassLoader
-            .getResourceAsStream("ttl_response.json")
+            ?.getResourceAsStream("ttc_buses_response.json")
         val mapper = ObjectMapper()
         val deserializer = BusesResponseParser()
-        val response = deserializer.deserialize(
+        val buses = deserializer.deserialize(
             mapper.factory.createParser(stream),
             mapper.deserializationContext
         )
-        val expectedBusLocations = listOf<Bus>(
-            Bus(Location(41.76619056790743, 44.77884267730354)),
-            Bus(Location(41.74894996470203, 44.777173414131894)),
+        val expectedBuses = listOf<Bus>(
+            Bus(Location(41.76619056790743, 44.77884267730354), Direction.Forward, 3527),
+            Bus(Location(41.74894996470203, 44.777173414131894), Direction.Backward, 903),
         )
-        val expectedResponse = Buses(expectedBusLocations)
-        assertEquals(expectedResponse, response)
+        val expectedResponse = Buses(expectedBuses)
+        assertEquals(expectedResponse, buses)
     }
 }
