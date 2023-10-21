55f3c3d297e772b0765efe8c01adab12f4a0f2f9
anagaf <aagafonov@arrival.com>
2023-10-21 21:06:34 +0400

    Improved AboutDialog

--- app/src/main/java/com/anagaf/tbilisibus/ui/AboutDialog.kt
+++ app/src/main/java/com/anagaf/tbilisibus/ui/AboutDialog.kt
@@ -1,11 +1,36 @@
 package com.anagaf.tbilisibus.ui
 
+import android.content.Intent
+import android.net.Uri
+import android.text.style.UnderlineSpan
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withAnnotation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat.startActivity
+import com.anagaf.tbilisibus.BuildConfig
 import com.anagaf.tbilisibus.R
+
+
+val kFontSize = 16.sp
 
 @Composable
 internal fun AboutDialog(
@@ -22,10 +47,50 @@ internal fun AboutDialog(
                     onDismissed()
                 }
             ) {
-                Text(text = stringResource(R.string.cancel))
+                Text(text = stringResource(R.string.ok))
             }
         },
-        text = { Text(text = "(c)Andrei Agafonov") },
+        text = {
+            val spacerSize = 12.dp
+            Column {
+                Text(fontSize = kFontSize, text = "Version ${BuildConfig.VERSION_NAME}")
+                Spacer(modifier = Modifier.size(spacerSize))
+                Text(fontSize = kFontSize, text = "Developed by Andrei Agafonov")
+                Spacer(modifier = Modifier.size(spacerSize))
+                IconsByText()
+            }
+        },
         onDismissRequest = {}
     )
+}
+
+@OptIn(ExperimentalTextApi::class)
+@Composable
+fun IconsByText() {
+    val icon8Tag = "icon8"
+    val text = buildAnnotatedString {
+        append("Icons by ")
+        withAnnotation(icon8Tag, "annotation") {
+            append(
+                AnnotatedString(
+                    "Icon8",
+                    spanStyle = SpanStyle(textDecoration = TextDecoration.Underline)
+                )
+            )
+        }
+    }
+    val uriHandler = LocalUriHandler.current
+    ClickableText(text = text, style = TextStyle(fontSize = kFontSize)) {
+        text.getStringAnnotations(it, it).firstOrNull()?.tag?.let { tag ->
+            if (tag == icon8Tag) {
+                uriHandler.openUri("https://icons8.com/")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AboutDialogPreview() {
+    AboutDialog {}
 }
